02b8b5027c5bc26c629dbdeb2191738ede6235d9
unknown <beiker2203@gmail.com>
2019-04-10 13:41:01 -0500

    Añadiendo Footer y modificando los link con handlebars en vez de vue

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => res.render('home'))
+const homeController = require('../controllers/home.controller')
+
+router.get('/', homeController.getIndex)
 router.get('/test', (req, res) => res.render('test'))
 
 module.exports = router
