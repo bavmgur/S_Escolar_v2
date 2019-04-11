d039af60c740ce322ad50eadf033e22a908f496d
Juan Manuel <juan.15121001@gmail.com>
2019-04-11 11:12:23 -0500

    creando los CRUD para los modelos

---
+++ routes/student.routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+const studentController = require('../controllers/index')
+
+// router.get('/', studentController.getIndex)
+
+module.exports = router
